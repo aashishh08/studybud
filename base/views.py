18dcf10aea012e7e924ce796742da09de293108f
Aashish Kumar <aashishkumar@Aashishs-Laptop.local>
2024-07-10 12:47:10 +0530

    3rd changes

--- base/views.py
+++ base/views.py
@@ -7,13 +7,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 
 
 def loginPage(request):
+    page = 'login'
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try: 
@@ -27,12 +29,30 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username/password does not exist')
-    context = {}
+    if request.user.is_authenticated:
+        return redirect('home')
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutPage(request):
     logout(request)
     return redirect('home')
+
+def registerpage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error has occured while registration')
+    context = {'page': page, 'form': form}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):

3cc731c6bd346feb8caacd8d75de3a53d402eec8
Aashish Kumar <aashishkumar@Aashishs-Laptop.local>
2024-07-10 19:44:45 +0530

    updated changes

--- base/views.py
+++ base/views.py
@@ -89,7 +89,10 @@ def room(request, pk):
 
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
-    context = {'user': user}
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms':rooms, 'topics': topics, 'room_messages': room_messages}
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
